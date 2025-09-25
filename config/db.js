a9935e90d64700f28f17e9f95e6cfdb296416f7d
MosesBurla <mosesburla@gmail.com>
2025-09-25 18:21:00 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI_ATLAS);
     console.log('MongoDB connected');
   } catch (err) {
     console.error(err);
